0b0fc92bef7141a3a11322477d26444edfc126b2
uthmaniv <uthmanyahayababa@gmail.com>
2025-09-29 10:46:45 +0100

    Added Response class for serialization

--- src/main/java/org/uthmaniv/service/Response.java
+++ src/main/java/org/uthmaniv/service/Response.java
@@ -1,4 +1,31 @@
 package org.uthmaniv.service;
 
 public class Response {
+
+    private final String status;
+    private final String message;
+    private final Object data;
+
+    public Response(String status, String message) {
+        this(status, message, null);
+    }
+
+    public Response(String status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
 }
